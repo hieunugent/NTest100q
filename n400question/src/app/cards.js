c1173f77845d8ca95abc6017cebc574acf94a89e
hieunugent <hieunugent@gmail.com>
2023-07-14 18:10:05 -0700

    update new ver. with ts

--- n400question/src/app/cards.js
+++
@@ -1,9 +0,0 @@
-import "../style/card.css"
-export default function Cards() {
-  return (
-    <div className="card">
-      <div className="card-title">Card lables</div>
-      <div className="card-content">card information that will be the question or the answer</div>
-    </div>
-  );
-}
